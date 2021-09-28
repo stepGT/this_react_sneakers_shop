e8754af6cffc31bfe777515c98859f96ff8154f3
Andrey <stepgt@mail.ru>
2021-09-28 10:59:42 +1000

    Added search block

--- src/App.js
+++ src/App.js
@@ -21,7 +21,13 @@ function App() {
         </ul>
       </header>
       <div className="content p-40">
-        <h1 className="mb-40">All sneakers</h1>
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>All sneakers</h1>
+          <div className="content__search_block d-flex">
+            <img src="/img/search.svg" alt="Search" />
+            <input placeholder="Search..." type="text" />
+          </div>
+        </div>
         <div className="d-flex">
           <div className="card">
             <img
