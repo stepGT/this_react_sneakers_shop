a9bee8a597ceb5127e44e5cccf75463a96c0a10e
Andrey <stepgt@mail.ru>
2021-09-29 12:02:52 +1000

    Added total block

--- src/App.js
+++ src/App.js
@@ -4,13 +4,38 @@ function App() {
       <div className="wrapper__overlay">
         <div className="wrapper__drawer">
           <h2 className="mb-30">Cart</h2>
-          <div className="wrapper__drawer_cart_item d-flex">
-            <div className="wrapper__drawer_cart_item_image" style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }}></div>
-            <div className="mr-20 flex">
-              <p className="mb-5">Мужские кроссовки NIKE Blazer Mid Suede</p>
-              <b>12999 &#8381;</b>
+          <div className="wrapper__drawer_cart_items">
+            <div className="wrapper__drawer_cart_item d-flex mb-20">
+              <div className="wrapper__drawer_cart_item_image" style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }}></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Мужские кроссовки NIKE Blazer Mid Suede</p>
+                <b>12999 &#8381;</b>
+              </div>
+              <img className="wrapper__drawer_cart_item_button" alt="Button" src="/img/btn-remove.svg" />
             </div>
-            <img className="wrapper__drawer_cart_item_button" alt="Button" src="/img/btn-remove.svg" />
+            <div className="wrapper__drawer_cart_item d-flex">
+              <div className="wrapper__drawer_cart_item_image" style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }}></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Мужские кроссовки NIKE Blazer Mid Suede</p>
+                <b>12999 &#8381;</b>
+              </div>
+              <img className="wrapper__drawer_cart_item_button" alt="Button" src="/img/btn-remove.svg" />
+            </div>
+          </div>
+          <div className="wrapper__drawer_cart_total_block">
+            <ul>
+              <li>
+                <span>Total:</span>
+                <div></div>
+                <b>21 456 &#8381;</b>
+              </li>
+              <li>
+                <span>Tax 5%:</span>
+                <div></div>
+                <b>1070 &#8381;</b>
+              </li>
+            </ul>
+            <button className="green_btn">Checkout <img src="/img/arrow.svg" alt="Arrow" /></button>
           </div>
         </div>
       </div>
