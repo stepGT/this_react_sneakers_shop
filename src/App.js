b91e4d8385a7393fbfa2e59bf7881ebc98ec8161
Andrey <stepgt@mail.ru>
2021-09-30 11:57:44 +1000

    Refactor Card component and output

--- src/App.js
+++ src/App.js
@@ -3,6 +3,33 @@ import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 import Search from "./components/Search";
 
+const sneakers = [
+  {
+    id: 1,
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 12999,
+    imageUrl: "img/sneakers/1.jpg",
+  },
+  {
+    id: 2,
+    title: "Мужские Кроссовки Nike Air Max 270",
+    price: 15600,
+    imageUrl: "img/sneakers/2.jpg",
+  },
+  {
+    id: 3,
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 8499,
+    imageUrl: "img/sneakers/3.jpg",
+  },
+  {
+    id: 4,
+    title: "Кроссовки Puma X Aka Boku Future Rider",
+    price: 8999,
+    imageUrl: "img/sneakers/4.jpg",
+  }
+];
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -14,7 +41,7 @@ function App() {
           <Search/>
         </div>
         <div className="d-flex">
-          <Card/>
+          {sneakers.map(el => <Card key={el.id} {...el} />)}
         </div>
       </div>
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 
-const Card = () => {
+const Card = (props) => {
     return (
         <div className="card">
             <img
                 width={133}
                 height={112}
-                src="/img/sneakers/1.jpg"
+                src={props.imageUrl}
                 alt="Sneakers"
             />
-            <h5>Мужские кроссовки NIKE Blazer Mid Suede</h5>
+            <h5>{props.title}</h5>
             <div className="d-flex justify-between align-center">
                 <div className="d-flex flex-column">
                     <span>Price</span>
-                    <b>12999 &#8381;</b>
+                    <b>{props.price} &#8381;</b>
                 </div>
                 <button className="card__button">
                     <img width={11} height={11} alt="" src="/img/plus.svg" />
