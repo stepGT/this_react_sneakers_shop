6043df3dcb37855af656bd7ba611b9dd00809f25
Andrey <stepgt@mail.ru>
2021-10-04 16:12:53 +1000

    Added needed classes

--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -38,7 +38,7 @@ const Drawer = (props) => {
                             </ul>
                             <button className={styles.greenBtn}>Checkout <img src="/img/arrow.svg" alt="Arrow" /></button>
                         </div>
-                    </> : <div class={styles.cartEmpty}>
+                    </> : <div class={`${styles.cartEmpty} d-flex align-center justify-center flex-column flex`}>
                         <img class="mb-20" width="120px" height="120px" src="/img/empty-cart.jpg" alt="Empty" />
                         <h2>Cart is empty</h2>
                         <p class="opacity-6">Add at least one pair of sneakers to order.</p>
