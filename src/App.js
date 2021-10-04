00403c908691e94e1868054dcbc28385b3649e27
Andrey <stepgt@mail.ru>
2021-10-04 12:33:36 +1000

    Added search handler

--- src/App.js
+++ src/App.js
@@ -8,9 +8,13 @@ function App() {
   const [isOpened, setIsOpened] = useState(false);
   const [sneakers, setSneakers] = useState([]);
   const [cartSneakers, setCartSneakers] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
   const onAddToCart = (obj) => {
     setCartSneakers(prev => ([...prev, obj]))
   }
+  const onChangeSearchInput = (e) => {
+    setSearchValue(e.target.value)
+  } 
   //
   useEffect(() => {
     fetch('https://615684b5e039a0001725aa2e.mockapi.io/items')
@@ -23,11 +27,11 @@ function App() {
       <Header onClickCard={() => setIsOpened(true)} />
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
-          <h1>All sneakers</h1>
-          <Search/>
+          <h1>{ searchValue ? `Search value: ${searchValue}` : 'All sneakers' }</h1>
+          <Search onClickRemove={() => setSearchValue('')} searchValue={searchValue} onChangeEvent={onChangeSearchInput} />
         </div>
         <div className="d-flex flex-wrap">
-          {sneakers.map(el => <Card
+          {sneakers.filter(item => item.title.toLowerCase().includes(searchValue)).map(el => <Card
             onPlus={(obj) => onAddToCart(obj)}
             key={el.id} 
             {...el} />)
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from './Search.module.scss';
 
-const Search = () => {
+const Search = ({ searchValue, onChangeEvent, onClickRemove }) => {
     return <div className={styles.searchBlock}>
         <img src="/img/search.svg" alt="Search" />
-        <input placeholder="Search..." type="text" />
+        {searchValue && <img onClick={onClickRemove} className={styles.clear} src="/img/btn-remove.svg" alt="Remove" />}
+        <input value={searchValue} onChange={onChangeEvent} placeholder="Search..." type="text" />
     </div>
 }
 
