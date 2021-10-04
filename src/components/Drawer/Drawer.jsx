53182342eb2518a58ae268dd25eb7e61d14ec851
Andrey <stepgt@mail.ru>
2021-10-04 16:01:44 +1000

    Added empty cart

--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -8,6 +8,15 @@ const Drawer = (props) => {
                 <h2 className="mb-30 d-flex justify-between">Cart
                     <img onClick={props.onClose} className={styles.buttonRemove} alt="Button" src="/img/btn-remove.svg" />
                 </h2>
+                <div class={styles.cartEmpty}>
+                    <img class="mb-20" width="120px" height="120px" src="/img/empty-cart.jpg" alt="Empty" />
+                    <h2>Cart is empty</h2>
+                    <p class="opacity-6">Add at least one pair of sneakers to order.</p>
+                    <button class={`${styles.greenButton} ${styles.greenBtn}`}>
+                        <img src="/img/arrow.svg" alt="Arrow" />
+                        Back
+                    </button>
+                </div>
                 <div className={styles.cartItems}>
                     {props.cartSneakers.map(item => {
                         return <div className={styles.cartItem}>
