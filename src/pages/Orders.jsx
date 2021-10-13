f8ad22d629bd75624bc7f04515bbfa090d622aac
Andrey <stepgt@mail.ru>
2021-10-13 14:57:47 +1000

    Get orders from backend

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import axios from 'axios';
+import Card from '../components/Card/Card';
 
 const Orders = () => {
+    const [orders, setOrders] = React.useState([]);
+    React.useEffect(() => {
+        (async () => {
+            try {
+                const { data } = await axios.get(`${process.env.REACT_APP_MOCKAPI_URL}/orders`);
+                setOrders(data.reduce((prev, obj) => [...prev, ...obj.items], []));
+            } catch (error) {
+                alert('Error! Get orders', error)
+            }
+        })()
+    }, []);
     return (
         <div className="content p-40">
             <div className="d-flex align-center justify-between mb-40">
@@ -8,9 +21,10 @@ const Orders = () => {
             </div>
             <div className="d-flex flex-wrap">
                 {
-                    [].map((el) => {
-                        console.log(el)
-                    })
+                    orders.map((el, ind) => <Card
+                    key={ind}
+                    {...el}
+                    />)
                 }
             </div>
         </div>
